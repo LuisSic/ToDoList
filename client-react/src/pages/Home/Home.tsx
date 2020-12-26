8efe49aeff759c5241413ce3e4e0268b1dd3632a
LuisSic <lantoni24@hotmail.com>
2020-12-25 21:07:19 -0600

    Refactor UI/UX

--- client-react/src/pages/Home/Home.tsx
+++ client-react/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Button from '../../components/Button/Button';
 import { ReactComponent as Add } from '../../img/add-outline.svg';
 import { ReactComponent as Alert } from '../../img/alert-circle-outline.svg';
 import { ReactComponent as Chatbox } from '../../img/chatbox-outline.svg';
@@ -13,14 +14,14 @@ import { ReactComponent as Person } from '../../img/person-add-outline.svg';
 import { ReactComponent as Refresh } from '../../img/refresh-outline.svg';
 import { ReactComponent as Settings } from '../../img/settings-outline.svg';
 import { ReactComponent as Chart } from '../../img/stats-chart-outline.svg';
-import { ReactComponent as Photo1 } from '../../img/photo-1.svg';
-import { ReactComponent as Photo2 } from '../../img/photo-2.svg';
+import { ReactComponent as Photo1 } from '../../img/photo-2.svg';
+import { ReactComponent as Photo2 } from '../../img/photo-1.svg';
 import { ReactComponent as Linkedin } from '../../img/linkedin.svg';
 import { ReactComponent as Microsoft } from '../../img/microsoft.svg';
-import { ReactComponent as Tesla } from '../../img/tesla-9.svg';
+import { ReactComponent as Tesla } from '../../img/coca-cola.svg';
 import { ReactComponent as Xbox } from '../../img/xbox-2.svg';
 import { ReactComponent as Facebook } from '../../img/facebook-1.svg';
-import { ReactComponent as Stripe } from '../../img/stripe-3.svg';
+import { ReactComponent as Netflix } from '../../img/netflix-2015-logo.svg';
 import { ReactComponent as Youtube } from '../../img/youtube.svg';
 import { ReactComponent as Sendgrid } from '../../img/sendgrid-2.svg';
 
@@ -32,17 +33,21 @@ const Home = () => {
         <Photo2 className="slogan__photo--2" />
         <div className="slogan__text">
           <h1 className="heading-1">
-            <span className="heading-1--main">Organize it all</span>
-            <span className="heading-1--sub">with To Do</span>
+            <span className="heading-1--main">Organize it all with To Do</span>
           </h1>
           <h2 className="heading-2">The all-in-one-to-do</h2>
+          <Button
+            className="btn slogan-btn"
+            text="Get Started"
+            callback={() => console.log('click')}
+          />
         </div>
       </div>
       <section className="features">
         <div className="features__header">
-          <h2 className="heading-2 features__header--center">
+          <h1 className="heading-1 features__header--center">
             Features that people love
-          </h2>
+          </h1>
           <Photo1 className="features__header--img" />
         </div>
         <div className="feature__header">
@@ -158,7 +163,7 @@ const Home = () => {
         <Linkedin className="customers__item--1 customers__img" />
         <Microsoft className="customers__item--2 customers__img" />
         <Xbox className="customers__item--3 customers__img" />
-        <Stripe className="customers__item--4 customers__img" />
+        <Netflix className="customers__item--4 customers__img" />
 
         <Tesla className="customers__item--5 customers__img" />
 
